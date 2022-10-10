215c470d52bb35ffcadb2d913ac74955e66b8728
mara666 <dario.sjs@gmail.com>
2022-10-10 00:37:26 -0300

    TG-W08: modify deleteAdmin function and add module.exports

--- src/resources/admins.js
+++ src/resources/admins.js
@@ -1,11 +1,11 @@
 const fs = require('fs');
 const admins = require('../data/admins.json');
 
-export const getAllAdmins = (req, res) => {
+const getAllAdmins = (req, res) => {
   res.send((admins));
 };
 
-export const getAdminsById = (req, res) => {
+const getAdminsById = (req, res) => {
   const adminId = req.params.id;
   const oneAdmin = admins.find((admin) => admin.id === Number(adminId));
   if (oneAdmin) {
@@ -15,7 +15,7 @@ export const getAdminsById = (req, res) => {
   }
 };
 
-export const addAdmin = (req, res) => {
+const addAdmin = (req, res) => {
   const newAdmin = req.body;
   admins.push(newAdmin);
   fs.writeFile('src/data/admins.json', JSON.stringify(admins), (err) => {
@@ -27,14 +27,25 @@ export const addAdmin = (req, res) => {
   });
 };
 
-export const deleteAdmin = (req, res) => {
-  const adminId = req.params.id;
-  const filteredAdmin = admins.filter((admin) => admin.id !== parseInt(adminId, 10));
+const deleteAdmin = (req, res) => {
+  const adminId = Number(req.params.id);
+  const filteredAdmin = admins.filter((admin) => admin.id !== adminId);
+  const oneAdmin = admins.find((admin) => admin.id === adminId);
+  if (!oneAdmin) {
+    res.send(`Cannot delete Admin with id ${req.params.id}, because it doesent exist!`);
+  }
   fs.writeFile('src/data/admins.json', JSON.stringify(filteredAdmin), (err) => {
     if (err) {
-      res.send(`Cannot delete Admin with id ${req.params.id}, please check!`);
+      res.send('An error has ocurred, please check!');
     } else {
       res.send('Admin has been deleted!');
     }
   });
+};
+
+module.exports = {
+  getAllAdmins,
+  getAdminsById,
+  addAdmin,
+  deleteAdmin,
 };
