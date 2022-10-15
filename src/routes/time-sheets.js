a715028134732fbfcf644a050f9c3d5d7bc772df
mara666 <dario.sjs@gmail.com>
2022-10-15 19:31:17 -0300

    TG-12: set timesheets post route

--- src/routes/time-sheets.js
+++ src/routes/time-sheets.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import {
-  getAllTimeSheets, getTimeSheetById,
+  getAllTimeSheets,
+  getTimeSheetById,
+  createTimeSheet,
 } from '../controllers/time-sheets';
 
 const router = express.Router();
 
 router
   .get('/', getAllTimeSheets)
-  .get('/:id', getTimeSheetById);
+  .get('/:id', getTimeSheetById)
+  .post('/', createTimeSheet);
 
 export default router;
