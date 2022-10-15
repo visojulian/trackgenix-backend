5fc686e135175ea2b9903f643e67bccfebdd1301
julianpiaggio <julianpiaggio@gmail.com>
2022-10-15 18:19:53 -0300

    TG-10: add validation new task creation

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import taskControllers from '../controllers/tasks';
-// import tasksValidations from '../validations/tasks';
+import tasksValidations from '../validations/tasks';
 
 const router = express.Router();
 
 router.get('/', taskControllers.getAllTasks);
 router.get('/:id', taskControllers.getTasksById);
-router.post('/', taskControllers.createTask);
+router.post('/', tasksValidations.validateCreationTask, taskControllers.createTask);
 
 export default router;
---
+++ src/validations/tasks.js
@@ -0,0 +1,21 @@
+import Joi from 'joi';
+
+const validateCreationTask = (req, res, next) => {
+  const taskValidation = Joi.object({
+    description: Joi.string().max(150).required(),
+  });
+
+  const validation = taskValidation.validate(req.body);
+
+  if (validation.error) {
+    return res.status(400).json({
+      message: `an error occurred: ${validation.error.details[0].message}`,
+      data: undefined,
+      error: true,
+    });
+  }
+  return next();
+};
+export default {
+  validateCreationTask,
+};
