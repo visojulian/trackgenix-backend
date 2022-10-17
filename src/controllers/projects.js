fcda1e9ca4c8e7102b0ef5ca653a11fddefa70dc
Mateo Scarabino <91098568+matescarabino@users.noreply.github.com>
2022-10-16 23:53:27 -0300

    TG-06: add null/undefined validation to find and findBy methods

--- src/controllers/projects.js
+++ src/controllers/projects.js
@@ -4,6 +4,13 @@ export const getProjects = async (req, res) => {
   try {
     const projects = await Projects.find(req.query);
 
+    if (!projects) {
+      return res.status(404).json({
+        message: 'Projects not found',
+        error: true,
+      });
+    }
+
     return res.status(200).json({
       message: 'All Projects',
       data: projects,
@@ -21,6 +28,13 @@ export const getProjectById = async (req, res) => {
   try {
     const { id } = req.params;
     const projects = await Projects.findById(id);
+
+    if (!projects) {
+      return res.status(404).json({
+        message: `Project ${req.params.id} does not exist`,
+        error: true,
+      });
+    }
 
     return res.status(200).json({
       message: `Project ${id} found`,
