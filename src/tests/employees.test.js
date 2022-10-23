0d2218e7ccf1babb48d40f370d5a7d8c81c4abff
Tomas Bettini <tomas.bettini98@gmail.com>
2022-10-23 20:34:19 -0300

    TG-20: add tests for get by id an employee

--- src/tests/employees.test.js
+++ src/tests/employees.test.js
@@ -17,4 +17,24 @@ describe('Time-sheet - Unit tests', () => {
       expect(response.body.message).toBe('Employees found');
     });
   });
+
+  describe('GET BY ID /:id', () => {
+    test('Should return status code 200', async () => {
+      const response = await request(app).get('/employees/634d5803354e41cd60b9e400').send();
+      expect(response.status).toBe(200);
+      expect(response.body.error).toBeFalsy();
+      expect(response.body.data).toBeDefined();
+      expect(response.body.message).toBe('Employee found');
+    });
+  });
+
+  describe('GET BY ID ERROR /:id', () => {
+    test('Should return status code 404', async () => {
+      const response = await request(app).get('/time-sheets/634d5803354e41cd60b9e555').send();
+      expect(response.status).toBe(404);
+      expect(response.body.error).toBeTruthy();
+      expect(response.body.data).toBeUndefined();
+      expect(response.body.message).toBe(`${response.body.message}`);
+    });
+  });
 });
