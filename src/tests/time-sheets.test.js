efdf995e863a227b2d804177590d1850218e86f8
mara666 <dario.sjs@gmail.com>
2022-10-23 17:13:31 -0300

    TG-21: add tests for getAll time-sheets

--- src/tests/time-sheets.test.js
+++ src/tests/time-sheets.test.js
@@ -1,5 +1,20 @@
-describe('example', () => {
-  it('Test', () => {
-    expect(true).toEqual(true);
+import request from 'supertest';
+import app from '../app';
+import TimeSheets from '../models/Time-sheets';
+import timeSheetsSeed from '../seeds/time-sheets';
+
+describe('Time-sheet - Unit tests', () => {
+  beforeAll(async () => {
+    await TimeSheets.collection.insertMany(timeSheetsSeed);
+  });
+
+  describe('GET /time-sheets', () => {
+    test('should return status code 200', async () => {
+      const response = await request(app).get('/time-sheets').send();
+      expect(response.status).toBe(200);
+      expect(response.body.error).toBeFalsy();
+      expect(response.body.data).toBeDefined();
+      expect(response.body.message).toBe('Time sheets found');
+    });
   });
 });
