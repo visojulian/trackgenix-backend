f874a079b1c1ace6922743618c424ec3ef31eefe
mara666 <dario.sjs@gmail.com>
2022-10-23 17:55:57 -0300

    TG-21: add tests for getById for time-sheets

--- src/tests/time-sheets.test.js
+++ src/tests/time-sheets.test.js
@@ -17,4 +17,24 @@ describe('Time-sheet - Unit tests', () => {
       expect(response.body.message).toBe('Time sheets found');
     });
   });
+
+  describe('GET BY ID /:id', () => {
+    test('should return status code 200', async () => {
+      const response = await request(app).get('/time-sheets/634adaf83e995e6b4c7864a7').send();
+      expect(response.status).toBe(200);
+      expect(response.body.error).toBeFalsy();
+      expect(response.body.data).toBeDefined();
+      expect(response.body.message).toBe('Time sheet found');
+    });
+  });
+
+  describe('GET BY ID ERROR /:id', () => {
+    test('should return status code 400', async () => {
+      const response = await request(app).get('/time-sheets/634adaf83e995e6b4c7864z1').send();
+      expect(response.status).toBe(400);
+      expect(response.body.error).toBeTruthy();
+      expect(response.body.data).toBeUndefined();
+      expect(response.body.message).toBe(`${response.body.message}`);
+    });
+  });
 });
