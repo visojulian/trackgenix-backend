d6118ebc1df6ec019e82bd32fbac61b91a33385c
Mateo Scarabino <91098568+matescarabino@users.noreply.github.com>
2022-10-23 20:38:12 -0300

    TG-26: create delete and delete error test

--- src/tests/projects.test.js
+++ src/tests/projects.test.js
@@ -1,5 +1,40 @@
-describe('GET /projects', () => {
-  it('Test', () => {
-    expect(true).toEqual(true);
+import request from 'supertest';
+import app from '../app';
+import Projects from '../models/Projects';
+import projectSeed from '../seeds/projects';
+
+describe('Project - Unit tests', () => {
+  beforeAll(async () => {
+    await Projects.collection.insertMany(projectSeed);
+  });
+
+  describe('GET /projects', () => {
+    test('should return status code 200', async () => {
+      const response = await request(app).get('/projects').send();
+      expect(response.status).toBe(200);
+      expect(response.body.error).toBeFalsy();
+      expect(response.body.data).toBeDefined();
+      expect(response.body.message).toBe('All Projects');
+    });
+  });
+
+  describe('DELETE /:id', () => {
+    test('should return status code 200', async () => {
+      const response = await request(app).delete('/projects/6354c31fa0f546fd19325575').send();
+      expect(response.status).toBe(200);
+      expect(response.body.error).toBeFalsy();
+      expect(response.body.data).toBeDefined();
+      expect(response.body.message).toBe(`${response.body.message}`);
+    });
+  });
+
+  describe('DELETE ERROR /:id', () => {
+    test('should return status code 404', async () => {
+      const response = await request(app).delete('/projects/6354c31fa0f546fd19325570').send();
+      expect(response.status).toBe(404);
+      expect(response.body.error).toBeTruthy();
+      expect(response.body.data).toBeUndefined();
+      expect(response.body.message).toBe(`${response.body.message}`);
+    });
   });
 });
