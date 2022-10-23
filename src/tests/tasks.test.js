99466191b8cc0dba7f663225f86b9760414e10d8
Julieta_Balcaza <julibalcaza15@gmail.com>
2022-10-23 02:11:07 -0300

    TG-16: create GET test for tasks

--- src/tests/tasks.test.js
+++ src/tests/tasks.test.js
@@ -1,5 +1,34 @@
-describe('example', () => {
-  it('Test', () => {
-    expect(true).toEqual(true);
+// describe('example', () => {
+//   it('Test', () => {
+//     expect(true).toEqual(true);
+//   });
+// });
+
+import request from 'supertest';
+import app from '../app';
+import Tasks from '../models/Tasks';
+import TasksSeed from '../seeds/tasks';
+
+beforeAll(async () => {
+  await Tasks.collection.insertMany(TasksSeed);
+});
+
+describe('GET /tasks', () => {
+  test('Should return status code 200', async () => {
+    const response = await request(app).get('/tasks').send();
+
+    expect(response.status).toBe(200);
+  });
+
+  test('Should return error false', async () => {
+    const response = await request(app).get('/tasks').send();
+
+    expect(response.body.error).toBe(false);
+  });
+
+  test('Should return more than one task', async () => {
+    const response = await request(app).get('/tasks').send();
+
+    expect(response.body.data.length).toBeGreaterThan(0);
   });
 });
