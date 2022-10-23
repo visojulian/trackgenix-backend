8306a6c35a7e77492d6e56f750141d2b36e476f4
Tomas Bettini <tomas.bettini98@gmail.com>
2022-10-23 20:20:56 -0300

    TG-20: add tests for get all employees

--- src/tests/employees.test.js
+++ src/tests/employees.test.js
@@ -1,5 +1,20 @@
-describe('example', () => {
-  it('Test', () => {
-    expect(true).toEqual(true);
+import request from 'supertest';
+import app from '../app';
+import Employees from '../models/Employees';
+import employeesSeed from '../seeds/employees';
+
+describe('Time-sheet - Unit tests', () => {
+  beforeAll(async () => {
+    await Employees.collection.insertMany(employeesSeed);
+  });
+
+  describe('GET /employees', () => {
+    test('Should return status code 200', async () => {
+      const response = await request(app).get('/employees').send();
+      expect(response.status).toBe(200);
+      expect(response.body.error).toBeFalsy();
+      expect(response.body.data).toBeDefined();
+      expect(response.body.message).toBe('Employees found');
+    });
   });
 });
