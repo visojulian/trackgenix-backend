5ea33e79b41796b2ccc565ed4b0d628ed9535abf
julianpiaggio <julianpiaggio@gmail.com>
2022-10-24 14:28:39 -0300

    TG-27: make test create projects

--- src/tests/projects.test.js
+++ src/tests/projects.test.js
@@ -1,4 +1,5 @@
 import request from 'supertest';
+import mongoose from 'mongoose';
 import app from '../app';
 import Projects from '../models/Projects';
 import ProjectsSeed from '../seeds/projects';
@@ -29,6 +30,44 @@ describe('Projects - Unit tests', () => {
 
     test('should return status code 400', async () => {
       const response = await request(app).get('/projects/6354c2ee2c85bbe8fb51072').send();
+      expect(response.status).toBe(400);
+      expect(response.body.error).toBeTruthy();
+      expect(response.body.data).toBeUndefined();
+      expect(response.body.message).toBe(`${response.body.message}`);
+    });
+  });
+
+  describe('POST /projects', () => {
+    test('should create a project and return status 201', async () => {
+      const projectsMooked = {
+        name: 'Project11',
+        description: 'Test11',
+        startDate: '2022-12-13',
+        endDate: '2022-12-18',
+        clientName: 'pepita',
+        employees: [{
+          employee: mongoose.Types.ObjectId('634d5803354e41cd60b9e422'),
+          role: 'QA',
+          rate: 5.2,
+        }],
+      };
+      const response = await request(app).post('/projects').send(projectsMooked);
+      expect(response.status).toBe(201);
+      expect(response.body.error).toBeFalsy();
+      expect(response.body.data).toBeDefined();
+      expect(response.body.message).toBe('Project created successfully!');
+    });
+
+    test('should not create a project and return status 400', async () => {
+      const projectsMooked = {
+        name: '',
+        description: '',
+        startDate: '',
+        endDate: '',
+        clientName: '',
+        employees: [{ employee: '' }],
+      };
+      const response = await request(app).post('/projects').send(projectsMooked);
       expect(response.status).toBe(400);
       expect(response.body.error).toBeTruthy();
       expect(response.body.data).toBeUndefined();
