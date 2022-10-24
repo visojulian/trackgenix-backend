2d2f083fcae9641f4cba2d159dc1ba49a4f893e2
Tomas Bettini <tomas.bettini98@gmail.com>
2022-10-24 11:17:01 -0300

    TG-20: add tests for create an employee

--- src/tests/employees.test.js
+++ src/tests/employees.test.js
@@ -30,11 +30,112 @@ describe('Time-sheet - Unit tests', () => {
 
   describe('GET BY ID ERROR /:id', () => {
     test('Should return status code 404', async () => {
-      const response = await request(app).get('/time-sheets/634d5803354e41cd60b9e555').send();
+      const response = await request(app).get('/employees/634d5803354e41cd60b9e555').send();
       expect(response.status).toBe(404);
+      expect(response.body.data).toBeUndefined();
+      expect(response.body.message).toBe('This employee id: 634d5803354e41cd60b9e555 does not exists');
+    });
+  });
+
+  describe('POST /employees', () => {
+    test('Should return status code 201', async () => {
+      const employeeMocked = {
+        name: 'Tomas',
+        lastName: 'Bettini',
+        phone: '9512613592',
+        email: 'tomasbettini@gmail.com',
+        password: '591gJWFkIlF8',
+      };
+      const response = await request(app).post('/employees').send(employeeMocked);
+      expect(response.status).toBe(201);
+      expect(response.body.error).toBeFalsy();
+      expect(response.body.data).toBeDefined();
+      expect(response.body.message).toBe('Employee created successfully');
+    });
+  });
+
+  describe('POST ERROR Name Required /employees', () => {
+    test('Should return status code 400', async () => {
+      const employeeMocked = {
+        name: '',
+        lastName: 'Bettini',
+        phone: '9512613592',
+        email: 'tomasbettini@gmail.com',
+        password: '591gJWFkIlF8',
+      };
+      const response = await request(app).post('/employees').send(employeeMocked);
+      expect(response.status).toBe(400);
       expect(response.body.error).toBeTruthy();
       expect(response.body.data).toBeUndefined();
-      expect(response.body.message).toBe(`${response.body.message}`);
+      expect(response.body.message).toBe('Something went wrong: "name" is not allowed to be empty ');
+    });
+  });
+
+  describe('POST ERROR Last Name Required /employees', () => {
+    test('Should return status code 400', async () => {
+      const employeeMocked = {
+        name: 'Tomas',
+        lastName: '',
+        phone: '9512613592',
+        email: 'tomasbettini@gmail.com',
+        password: '591gJWFkIlF8',
+      };
+      const response = await request(app).post('/employees').send(employeeMocked);
+      expect(response.status).toBe(400);
+      expect(response.body.error).toBeTruthy();
+      expect(response.body.data).toBeUndefined();
+      expect(response.body.message).toBe('Something went wrong: "lastName" is not allowed to be empty ');
+    });
+  });
+
+  describe('POST ERROR Phone Required /employees', () => {
+    test('Should return status code 400', async () => {
+      const employeeMocked = {
+        name: 'Tomas',
+        lastName: 'Bettini',
+        phone: '',
+        email: 'tomasbettini@gmail.com',
+        password: '591gJWFkIlF8',
+      };
+      const response = await request(app).post('/employees').send(employeeMocked);
+      expect(response.status).toBe(400);
+      expect(response.body.error).toBeTruthy();
+      expect(response.body.data).toBeUndefined();
+      expect(response.body.message).toBe('Something went wrong: "phone" is not allowed to be empty ');
+    });
+  });
+
+  describe('POST ERROR Email Required /employees', () => {
+    test('Should return status code 400', async () => {
+      const employeeMocked = {
+        name: 'Tomas',
+        lastName: 'Bettini',
+        phone: '9512613592',
+        email: '',
+        password: '591gJWFkIlF8',
+      };
+      const response = await request(app).post('/employees').send(employeeMocked);
+      expect(response.status).toBe(400);
+      expect(response.body.error).toBeTruthy();
+      expect(response.body.data).toBeUndefined();
+      expect(response.body.message).toBe('Something went wrong: "email" is not allowed to be empty ');
+    });
+  });
+
+  describe('POST ERROR Password Required /employees', () => {
+    test('Should return status code 400', async () => {
+      const employeeMocked = {
+        name: 'Tomas',
+        lastName: 'Bettini',
+        phone: '9512613592',
+        email: 'tomasbettini@gmail.com',
+        password: '',
+      };
+      const response = await request(app).post('/employees').send(employeeMocked);
+      expect(response.status).toBe(400);
+      expect(response.body.error).toBeTruthy();
+      expect(response.body.data).toBeUndefined();
+      expect(response.body.message).toBe('Something went wrong: "password" is not allowed to be empty ');
     });
   });
 });
