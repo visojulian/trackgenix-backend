d7b30e5bcc30f4b491c3e524bedf8a8579fb90f6
julianpiaggio <julianpiaggio@gmail.com>
2022-10-24 14:06:50 -0300

    TG-27: create getAll projects test

--- src/tests/projects.test.js
+++ src/tests/projects.test.js
@@ -1,5 +1,20 @@
-describe('GET /projects', () => {
-  it('Test', () => {
-    expect(true).toEqual(true);
+import request from 'supertest';
+import app from '../app';
+import Projects from '../models/Projects';
+import ProjectsSeed from '../seeds/projects';
+
+describe('Projects - Unit tests', () => {
+  beforeAll(async () => {
+    await Projects.collection.insertMany(ProjectsSeed);
+  });
+
+  describe('GET /projects', () => {
+    test('should return status code 200', async () => {
+      const response = await request(app).get('/projects').send();
+      expect(response.status).toBe(200);
+      expect(response.body.error).toBeFalsy();
+      expect(response.body.data).toBeDefined();
+      expect(response.body.message).toBe('All Projects');
+    });
   });
 });
