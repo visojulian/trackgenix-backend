4526207f763bac15a3b880ed72f672b8cf9ebf25
julianpiaggio <julianpiaggio@gmail.com>
2022-10-24 14:08:06 -0300

    TG-27: create getById projects test

--- src/tests/projects.test.js
+++ src/tests/projects.test.js
@@ -17,4 +17,22 @@ describe('Projects - Unit tests', () => {
       expect(response.body.message).toBe('All Projects');
     });
   });
+
+  describe('GET BY ID /projects/:id', () => {
+    test('should return status code 200', async () => {
+      const response = await request(app).get('/projects/6354c3046634d3f5d058bae8').send();
+      expect(response.status).toBe(200);
+      expect(response.body.error).toBeFalsy();
+      expect(response.body.data).toBeDefined();
+      expect(response.body.message).toBe(`${response.body.message}`);
+    });
+
+    test('should return status code 400', async () => {
+      const response = await request(app).get('/projects/6354c2ee2c85bbe8fb51072').send();
+      expect(response.status).toBe(400);
+      expect(response.body.error).toBeTruthy();
+      expect(response.body.data).toBeUndefined();
+      expect(response.body.message).toBe(`${response.body.message}`);
+    });
+  });
 });
