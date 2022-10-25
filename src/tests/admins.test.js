418332003af8479faeecb924b45ad59016f91b54
Valentin-sanabria <valensanabria2000@hotmail.com>
2022-10-24 21:18:10 -0300

    TG-15: Corrections based on PR feedback.

--- src/tests/admins.test.js
+++ src/tests/admins.test.js
@@ -25,10 +25,8 @@ describe('GET - Filter by ID', () => {
     expect(response.body.data).toBeTruthy();
     expect(response.body.message).toBe('Admin found');
   });
-});
 
-describe('ERROR GET - Filter by ID', () => {
-  test('should return status 400', async () => {
+  test('Should return status 400 when the id is invalid', async () => {
     const response = await request(app).get('/admins/634b30cda84415df76492ecf').send();
     expect(response.status).toBe(400);
     expect(response.body.error).toBe(true);
@@ -36,62 +34,62 @@ describe('ERROR GET - Filter by ID', () => {
   });
 });
 
-describe('ERRORS PUT - invalid edits', () => {
-  test('Should return "name empty" ', async () => {
-    const fakeAdmin = {
+describe('PUT - invalid edits', () => {
+  test('Should return an error when the name is empty', async () => {
+    const invalidAdmin = {
       name: '',
       lastName: 'not real',
       email: 'asd@hotm.com',
       password: '23132aaaaa',
     };
-
-    const response = await request(app).put('/admins/634b30cda844d15df73652exd').send(fakeAdmin);
+    const response = await request(app).put('/admins/634b30cda844d15df73652exd').send(invalidAdmin);
     expect(response.status).toBe(400);
     expect(response.body.message).toBe('There was an error: "name" is not allowed to be empty');
   });
 
-  test('Should return "lastName empty" ', async () => {
-    const fakeAdmin = {
+  test('Should return an error when the last name is empty', async () => {
+    const invalidAdmin = {
       name: 'asdasd',
       lastName: '',
       email: 'asd@hotm.com',
       password: '23132aaaaa',
     };
 
-    const response = await request(app).put('/admins/634b30cda844d15df73652exd').send(fakeAdmin);
+    const response = await request(app).put('/admins/634b30cda844d15df73652exd').send(invalidAdmin);
     expect(response.status).toBe(400);
     expect(response.body.message).toBe('There was an error: "lastName" is not allowed to be empty');
   });
 
-  test('Should return "email empty" ', async () => {
-    const fakeAdmin = {
+  test('Should return an error when the email is empty', async () => {
+    const invalidAdmin = {
       name: 'asdasd',
       lastName: 'not real',
       email: '',
       password: '23132aaaaa',
     };
 
-    const response = await request(app).put('/admins/634b31caa18bcb2e7eb97458').send(fakeAdmin);
+    const response = await request(app).put('/admins/634b31caa18bcb2e7eb97458').send(invalidAdmin);
     expect(response.status).toBe(400);
     expect(response.body.message).toBe('There was an error: "email" is not allowed to be empty');
   });
 
-  test('Should return "password empty" ', async () => {
-    const fakeAdmin = {
+  test('Should return an error when the password is empty', async () => {
+    const invalidAdmin = {
       name: 'asdasd',
       lastName: 'not real',
       email: 'idontexist@hot.com',
       password: '',
     };
 
-    const response = await request(app).put('/admins/634b31caa18bcb2e7eb97458').send(fakeAdmin);
+    const response = await request(app).put('/admins/634b31caa18bcb2e7eb97458').send(invalidAdmin);
     expect(response.status).toBe(400);
     expect(response.body.message).toBe('There was an error: "password" is not allowed to be empty');
   });
 
-  test('Should return "cast to objectId failed" ', async () => {
+  test('Should return an error when the id is invalid', async () => {
     const response = await request(app).put('/admins/634b31caa18bcb2e7eb97458').send();
     expect(response.status).toBe(400);
+    expect(response.body.error).toBe(true);
   });
 
   test('should return status code 200', async () => {
@@ -121,9 +119,7 @@ describe('POST - create admin', () => {
     expect(response.body.error).toBe(false);
     expect(response.body.message).toBe('Admin created');
   });
-});
 
-describe('ERROR POST - create empty admin', () => {
   const emptyAdmin = {
     name: '',
     lastName: '',
@@ -138,16 +134,14 @@ describe('ERROR POST - create empty admin', () => {
   });
 });
 
-describe('ERROR DELETE - invalid id', () => {
-  test('should return status 400', async () => {
+describe('DELETE tests', () => {
+  test('should return status 400 when the id is invalid', async () => {
     const response = await request(app).delete('/admins/634b30cda84415df73472ecf').send();
     expect(response.status).toBe(400);
     expect(response.body.error).toBe(true);
     expect(response.body.data).toBe(undefined);
   });
-});
 
-describe('DELETE - delete admin', () => {
   test('should return status 200', async () => {
     const response = await request(app).delete('/admins/634b30cda84415df73652ecf').send();
     expect(response.status).toBe(200);
