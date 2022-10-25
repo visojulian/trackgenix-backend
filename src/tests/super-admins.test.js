3e74d483e6ace74466dc497e2211717a2b4d41e9
julianpiaggio <julianpiaggio@gmail.com>
2022-10-25 16:05:00 -0300

    TG-18: create put test

--- src/tests/super-admins.test.js
+++ src/tests/super-admins.test.js
@@ -124,4 +124,62 @@ describe('Super-Admins - Unit tests', () => {
       expect(response.body.message).toContain('An error ocurred!');
     });
   });
+
+  describe('PUT /super-admins/:id', () => {
+    test('should return status code 200 when a super admin is edited', async () => {
+      const superAdminMooked = {
+        name: 'Percy',
+        lastName: 'Mickan',
+        email: 'lmickan9@sun.com',
+        password: 'ChangePassword1',
+      };
+      const response = await request(app).put('/super-admins/63557ff1d2da47ee05366c85').send(superAdminMooked);
+      expect(response.status).toBe(200);
+      expect(response.body.error).toBeFalsy();
+      expect(response.body.data).toBeDefined();
+      expect(response.body.message).toBe('Super admin was edited');
+    });
+
+    test('should return status code 400 when a super admin is not edited because it did not pass validation in name', async () => {
+      const superAdminMooked = {
+        name: '',
+        lastName: 'Mickan',
+        email: 'lmickan9@sun.com',
+        password: 'ChangePassword1',
+      };
+      const response = await request(app).put('/super-admins/63557ff1d2da47ee05366c85').send(superAdminMooked);
+      expect(response.status).toBe(400);
+      expect(response.body.error).toBeTruthy();
+      expect(response.body.data).toBeUndefined();
+      expect(response.body.message).toBe('There was an error: "name" is not allowed to be empty');
+    });
+
+    test('should return status code 400 when a super admin is not edited because it did not pass validation in email', async () => {
+      const superAdminMooked = {
+        name: 'Percy',
+        lastName: 'Mickan',
+        email: '',
+        password: 'ChangePassword1',
+      };
+      const response = await request(app).put('/super-admins/63557ff1d2da47ee05366c85').send(superAdminMooked);
+      expect(response.status).toBe(400);
+      expect(response.body.error).toBeTruthy();
+      expect(response.body.data).toBeUndefined();
+      expect(response.body.message).toBe('There was an error: "email" is not allowed to be empty');
+    });
+
+    test('should return status code 400 when a super admin is not edited because it did not pass validation in password', async () => {
+      const superAdminMooked = {
+        name: 'Percy',
+        lastName: 'Mickan',
+        email: 'lmickan9@sun.com',
+        password: '',
+      };
+      const response = await request(app).put('/super-admins/63557ff1d2da47ee05366c85').send(superAdminMooked);
+      expect(response.status).toBe(400);
+      expect(response.body.error).toBeTruthy();
+      expect(response.body.data).toBeUndefined();
+      expect(response.body.message).toBe('There was an error: "password" is not allowed to be empty');
+    });
+  });
 });
