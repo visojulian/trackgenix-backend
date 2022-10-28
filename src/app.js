3a674b941a41a13894a7f47eaf1716f6b16f79e9
Julian Viso <julian.viso.romero@gmail.com>
2022-10-28 11:21:08 -0300

    TG-28: implement cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import routes from './routes/index';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
 app.use(routes);
 
 export default app;
