ae8560a50722a1b4cf90800645256a36ae706f9b
Maxi Delfino <maxidelfino1234@hotmail.com>
2022-11-24 19:16:49 -0300

    TG-99: Validation messages for admins-name

--- src/validations/admins.js
+++ src/validations/admins.js
@@ -2,7 +2,21 @@ import Joi from 'joi';
 
 const validateAdminBody = (req, res, next) => {
   const adminValidation = Joi.object({
-    name: Joi.string().min(3).max(50).required(),
+    name: Joi.string()
+      .min(3)
+      .max(20)
+      .alphanum()
+      .pattern(/^([^0-9]*)$/i)
+      .trim()
+      .required()
+      .messages({
+        'string.empty': 'Name cannot be empty',
+        'string.min': 'Name must have at least 3 characters',
+        'string.max': 'Name cannot exceed 20 characters',
+        'string.alphanum': 'Name cannot not have special characters',
+        'string.pattern.base': 'Name can only have letters',
+        'string.required': 'Name is required',
+      }),
     lastName: Joi.string().min(3).max(50).required(),
     email: Joi.string().email().required(),
     password: Joi.string().pattern(/^[a-zA-Z0-9]{8,30}$/).required(),
