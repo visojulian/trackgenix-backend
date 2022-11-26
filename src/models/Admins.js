b317761f6a3716c72755d106299f5189eb9817d6
Julieta Preliasco Benitez <preliascojulieta@gmail.com>
2022-11-26 18:04:26 -0300

    TG-109: update schemas add firebaseUid

--- src/models/Admins.js
+++ src/models/Admins.js
@@ -7,6 +7,7 @@ const adminSchema = new Schema({
   lastName: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
+  firebaseUid: { type: String, required: true },
 });
 
 export default mongoose.model('Admins', adminSchema);
