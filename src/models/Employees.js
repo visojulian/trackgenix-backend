10c85f82ef10c1b4652a673f01bbf96ad280a156
Julian Viso <julian.viso.romero@gmail.com>
2022-12-19 15:42:39 -0300

    TG-151: fix phone validations

--- src/models/Employees.js
+++ src/models/Employees.js
@@ -5,7 +5,7 @@ const { Schema } = mongoose;
 const employeesSchema = new Schema({
   name: { type: String, required: true },
   lastName: { type: String, required: true },
-  phone: { type: Number, required: true },
+  phone: { type: String, required: true },
   email: { type: String, required: true },
   firebaseUid: { type: String, required: true },
 });
