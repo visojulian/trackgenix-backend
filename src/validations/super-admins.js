8bf19be33f572789b8d6c48f5cab480fa8f53e12
Camila Rojas <camilavaleriarojas1@gmail.com>
2022-12-21 12:01:29 -0300

    TG-187: fix name and last name on admin, employee and super admin validations

--- src/validations/super-admins.js
+++ src/validations/super-admins.js
@@ -1,3 +1,4 @@
+/* eslint-disable newline-per-chained-call */
 import Joi from 'joi';
 
 const validateSuperAdminBody = (req, res, next) => {
@@ -5,30 +6,26 @@ const validateSuperAdminBody = (req, res, next) => {
     name: Joi.string()
       .min(3)
       .max(20)
-      .alphanum()
       .trim()
-      .pattern(/^([^0-9]*)$/i)
+      .pattern(/^[a-zA-Z\s]*$/)
       .required()
       .messages({
         'string.empty': 'Name cannot be empty',
         'string.min': 'Name must have at least 3 characters',
         'string.max': 'Name cannot exceed 20 characters',
-        'string.alphanum': 'Name cannot not have special characters',
         'string.pattern.base': 'Name can only have letters',
         'string.required': 'Name is required',
       }),
     lastName: Joi.string()
       .min(3)
       .max(25)
-      .alphanum()
       .trim()
-      .pattern(/^([^0-9]*)$/i)
+      .pattern(/^[a-zA-Z\s]*$/)
       .required()
       .messages({
         'string.empty': 'Last Name cannot be empty',
         'string.min': 'Last Name must have at least 3 characters',
         'string.max': 'Last Name cannot exceed 25 characters',
-        'string.alphanum': 'Last Name cannot not have special characters',
         'string.pattern.base': 'Last Name can only have letters',
         'string.required': 'Last Name is required',
       }),
@@ -41,14 +38,12 @@ const validateSuperAdminBody = (req, res, next) => {
         'string.email': 'Email needs to be a valid email address',
         'string.required': 'Email is required',
       }),
-    password: Joi.string().min(8).alphanum().trim()
-      .required()
-      .messages({
-        'string.empty': 'Password cannot be empty',
-        'string.min': 'Password must have at least 8 characters',
-        'string.alphanum': 'Password cannot contain special characters',
-        'string.required': 'Password is required',
-      }),
+    password: Joi.string().min(8).alphanum().trim().required().messages({
+      'string.empty': 'Password cannot be empty',
+      'string.min': 'Password must have at least 8 characters',
+      'string.alphanum': 'Password cannot contain special characters',
+      'string.required': 'Password is required',
+    }),
   });
 
   const validations = superAdminValidations.validate(req.body);
@@ -67,30 +62,26 @@ const validateEditSuperAdmin = (req, res, next) => {
     name: Joi.string()
       .min(3)
       .max(20)
-      .alphanum()
       .trim()
-      .pattern(/^([^0-9]*)$/i)
+      .pattern(/^[a-zA-Z\s]*$/)
       .required()
       .messages({
         'string.empty': 'Name cannot be empty',
         'string.min': 'Name must have at least 3 characters',
         'string.max': 'Name cannot exceed 20 characters',
-        'string.alphanum': 'Name cannot not have special characters',
         'string.pattern.base': 'Name can only have letters',
         'string.required': 'Name is required',
       }),
     lastName: Joi.string()
       .min(3)
       .max(25)
-      .alphanum()
       .trim()
-      .pattern(/^([^0-9]*)$/i)
+      .pattern(/^[a-zA-Z\s]*$/)
       .required()
       .messages({
         'string.empty': 'Last Name cannot be empty',
         'string.min': 'Last Name must have at least 3 characters',
         'string.max': 'Last Name cannot exceed 25 characters',
-        'string.alphanum': 'Last Name cannot not have special characters',
         'string.pattern.base': 'Last Name can only have letters',
         'string.required': 'Last Name is required',
       }),
